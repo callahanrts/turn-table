46f7c598c40f349505d0ee1d9590d44fc83c69f8
Cody <callahanrts@gmail.com>
2016-05-30 10:21:33 -0700

    can import videos and playlists

--- imports/api/youtube.js
+++ imports/api/youtube.js
@@ -16,19 +16,49 @@ if (Meteor.isServer) {
 Meteor.methods({ 'youtube.search' (query) {
     let future = new Future();
 
-    listYoutube(future, {
-      part: "id",
-      type: "video",
-      maxResults: 15,
-      q: query
-    })
+    if(/v=/.test(query)){
+      importVideos(future, [getParameterByName('v', query)])
+    } else if(/list=/.test(query)) {
+      importPlaylist(future, getParameterByName('list', query))
+    } else {
+      searchYoutube(future, {
+        part: "id",
+        type: "video",
+        maxResults: 15,
+        q: query
+      })
+    }
 
     return future.wait();
   },
 
 });
 
-var listYoutube = (future, options) => {
+var importPlaylist = (future, id) => {
+  YoutubeApi.playlistItems.list({
+    playlistId: id,
+    part: 'id,contentDetails',
+    maxResults: 50
+  }, (err, data) => {
+    console.log(err, data)
+    let ids = _.compact(data.items.map((el) => {
+      return el.contentDetails.videoId;
+    }));
+
+    importVideos(future, ids);
+  })
+}
+
+var importVideos = (future, ids) => {
+  YoutubeApi.videos.list({
+    part: "id,snippet,contentDetails",
+    id: ids.join(',')
+  }, (err, data) => {
+    future['return'](data, err);
+  })
+}
+
+var searchYoutube = (future, options) => {
   options = options || {}
 
   YoutubeApi.search.list(options, (err, data) => {
@@ -42,4 +72,14 @@ var listYoutube = (future, options) => {
       future['return'](data, err);
     })
   })
+}
+
+var getParameterByName = (name, url) => {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
