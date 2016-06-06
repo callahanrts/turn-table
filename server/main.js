d538dfa96be052b8dc092acba8eb45cbb352d760
Cody <callahanrts@gmail.com>
2016-06-05 21:18:20 -0700

    meteor settings

--- server/main.js
+++ server/main.js
@@ -6,20 +6,25 @@ import '../imports/api/playlists.js';
 import '../imports/api/youtube.js';
 import '../imports/api/users.js';
 
+// const GOOGLE_API_KEY      = process.env.GOOGLE_API_KEY
+// const GOOGLE_APP_SECRET   = process.env.GOOGLE_APP_SECRET
+// const FACEBOOK_APP_ID     = process.env.FACEBOOK_APP_ID
+// const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET
+// const TWITTER_API_KEY     = process.env.TWITTER_API_KEY
+// const TWITTER_APP_SECRET  = process.env.TWITTER_APP_SECRET
+
 Meteor.startup(() => {
   // code to run on server at startup
   Future = Npm.require('fibers/future');
 
-  const GOOGLE_API_KEY      = process.env.GOOGLE_API_KEY
-  const GOOGLE_APP_SECRET   = process.env.GOOGLE_APP_SECRET
-  const FACEBOOK_APP_ID     = process.env.FACEBOOK_APP_ID
-  const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET
-  const TWITTER_API_KEY     = process.env.TWITTER_API_KEY
-  const TWITTER_APP_SECRET  = process.env.TWITTER_APP_SECRET
+  let gsettings = Meteor.settings.private.services.google;
+  configureService("google", gsettings.api_key, gsettings.secret);
 
-  configureService("google", GOOGLE_API_KEY, GOOGLE_APP_SECRET);
-  configureService("facebook", FACEBOOK_APP_ID, FACEBOOK_APP_SECRET);
-  configureService("twitter", TWITTER_API_KEY, TWITTER_APP_SECRET);
+  let fbsettings = Meteor.settings.private.services.facebook;
+  configureService("facebook", fbsettings.api_key, fbsettings.secret);
+
+  let twsettings = Meteor.settings.private.services.twitter;
+  configureService("twitter", twsettings.api_key, twsettings.secret);
 });
 
 var configureService = (service, clientId, secret) => {
